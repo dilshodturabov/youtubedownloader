46cbc2c630de61efce2aece3f5ec05856ad9adee
dilshodturabov <avlomberdioglid@gmail.com>
2023-10-01 22:48:36 +0500

    initial commit

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const app = express();
 
 const port = 2003;
 
+app.get('/',(res, req)=>{
+    res.send('hello world');
+});
+
 app.listen(port,()=>{
     console.log(`Server is running on port ${port}`);
 });
