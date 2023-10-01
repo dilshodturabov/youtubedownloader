2e48dbdd075e701e262acba4edfc97198f574aa3
dilshodturabov <avlomberdioglid@gmail.com>
2023-10-02 00:19:13 +0500

    some changes

--- index.js
+++ index.js
@@ -16,20 +16,20 @@ const bot = new Telegraf('5019891372:AAFr7v9R50XDTD6pArcbZqcPx9JaYOFEVvk');
 const chatData = new Map();
 
 bot.start((ctx) => {
-    ctx.reply('Hello there! Welcome to my telegram bot.');
+    ctx.reply('Hello there! Welcome to my telegram bot. Send a Valid YouTube URL!');
 });
 
 bot.on('message', async (ctx) => {
-    ctx.reply('Fetching data...');
+    ctx.reply('Fetching data... 🎬');
     let url = ctx.message.text;
     if(ytdl.validateURL(url)) {
         let info = await ytdl.getInfo(url);
         let formats = ytdl.filterFormats(info.formats, 'videoandaudio');
         chatData.set(ctx.chat.id, { info, formats });
         let inlineKeyboard = formats.map((format, i) => [{ text: format.qualityLabel, callback_data: String(i) }]);
-        ctx.reply('Please choose a quality:', { reply_markup: { inline_keyboard: inlineKeyboard } });
+        ctx.reply('Please choose a video quality: 🖼️', { reply_markup: { inline_keyboard: inlineKeyboard } });
     } else {
-        ctx.reply('Please provide a valid YouTube URL.');
+        ctx.reply('Please provide a valid YouTube URL. 🔗');
     }
 });
 
@@ -37,6 +37,7 @@ bot.on('callback_query', (ctx) => {
     let i = parseInt(ctx.callbackQuery.data);
     let { info, formats } = chatData.get(ctx.chat.id);
     ctx.reply('Downloading...📥');
+    const streamOptions = { seek: 0, volume: 1 };
     const stream = ytdl.downloadFromInfo(info, { quality: formats[i].itag });
     ctx.replyWithVideo({ source: stream });
 });
